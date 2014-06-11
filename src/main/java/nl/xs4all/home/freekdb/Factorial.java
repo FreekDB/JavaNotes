4aceeb0dd6b598afd063b0ac8f39b48872a15150
Freek de Bruijn <freekdb@gmail.com>
2014-06-11 17:04:49 +0200

    Added Factorial class with unit tests - also added a pom.xml file and an IntelliJ module file (JavaNotes.iml).

---
+++ src/main/java/nl/xs4all/home/freekdb/Factorial.java
@@ -0,0 +1,22 @@
+package nl.xs4all.home.freekdb;
+
+import java.math.BigInteger;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Freek
+ * Date: 11-6-2014
+ * Time: 16:45
+ */
+public class Factorial {
+    public static void main(final String[] arguments) {
+        final BigInteger number = new BigInteger("42");
+        System.out.println(number + "! = " + new Factorial().factorial(number));
+    }
+
+    public BigInteger factorial(final BigInteger number) {
+        final BigInteger zero = BigInteger.ZERO;
+        final BigInteger one = BigInteger.ONE;
+        return number.compareTo(zero) <= 0 ? one : number.multiply(factorial(number.subtract(one)));
+    }
+}
---
+++ src/test/java/nl/xs4all/home/freekdb/FactorialTest.java
@@ -0,0 +1,25 @@
+package nl.xs4all.home.freekdb;
+
+import java.math.BigInteger;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Freek
+ * Date: 11-6-2014
+ * Time: 16:51
+ */
+public class FactorialTest {
+    @Test
+    public void testFactorial() {
+        final Factorial factorial = new Factorial();
+        assertEquals(BigInteger.ONE, factorial.factorial(BigInteger.ZERO));
+        assertEquals(new BigInteger("120"), factorial.factorial(new BigInteger("5")));
+        assertEquals(new BigInteger("3628800"), factorial.factorial(BigInteger.TEN));
+        final BigInteger prettyBigInteger = new BigInteger("1405006117752879898543142606244511569936384000000000");
+        assertEquals(prettyBigInteger, factorial.factorial(new BigInteger("42")));
+    }
+}
